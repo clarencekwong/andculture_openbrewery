59559c2b2324e35b0e6be3f3598b94e5148fab3b
github username <clarencekwong7@gmail.com>
2019-07-18 19:28:52 -0400

    pushing template

---
+++ src/components/BreweryList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import Brewery from './Brewery'
+
+class BreweryList extends React.Component {
+  render () {
+    return (
+      <Brewery />
+    )
+  }
+}
+
+export default BreweryList;
